712f3aa4947ccf2b27713e436d618b0d52677405
Da-CaRo <dcapi25.cal@gmail.com>
2025-05-27 21:42:37 +0200

    Add POC files

---
+++ htdocs/index.view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Actual Trivia</title>
+    <meta charset="utf-8">
+    <link href="styles/default.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+</body>
+
+</html>
