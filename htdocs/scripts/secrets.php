d27ab1e57ef8aaf98159ae2b27c2668ab6d5a770
Da-CaRo <dcapi25.cal@gmail.com>
2025-06-03 21:13:16 +0200

    Add dummy `secrets.php` file

---
+++ htdocs/scripts/secrets.php
@@ -0,0 +1,8 @@
+<?php
+function secrets() {
+    $secrets['mysql_server'] = 'dummy';
+    $secrets['mysql_user'] = 'dummy';
+    $secrets['mysql_password'] = 'dummy';
+    $secrets['mysql_db'] = 'dummy';
+    return $secrets;
+}
